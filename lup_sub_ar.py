6531723cdd6a1e09afe7d835b3be4fa3a52b0b2d
Andres Jasso <qc.test.dev@gmail.com>
2024-07-11 14:34:14 -0600

    Excel done with subregion and dispo

--- lup_sub_ar.py
+++ lup_sub_ar.py
@@ -4,7 +4,9 @@ import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import openpyxl
 from openpyxl.styles import PatternFill, Font, Border, Side
+import os
 import time
+import sys
 
 # Deshabilitar advertencias de solicitudes inseguras
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -100,13 +102,18 @@ def write_to_excel(channels, total, file_name='epg_data.xlsx'):
 
         # Calcular y mostrar el porcentaje de progreso
         progress = (index - 1) / total_channels * 100
-        print(f"Progreso: {progress:.2f}% completado", end='\r')
+        sys.stdout.write(f"\rProgreso: {progress:.2f}% completado")
+        sys.stdout.flush()
         time.sleep(0.01)  # Para simular tiempo de procesamiento
 
     sheet.cell(row=total_channels + 2, column=1, value="Total de canales:").font = Font(bold=True)
     sheet.cell(row=total_channels + 2, column=2, value=total).border = border
 
-    workbook.save(file_name)
+    # Guardar el archivo en la carpeta de descargas
+    downloads_folder = os.path.expanduser("~/Downloads")
+    file_path = os.path.join(downloads_folder, file_name)
+    workbook.save(file_path)
+    print(f"\nArchivo guardado en {file_path}")
 
 if __name__ == "__main__":
     device_type_input = input("Android Mobile : ADR M \n"
@@ -116,7 +123,6 @@ if __name__ == "__main__":
                               "Roku : Rk \n"
                               "Ingrese el dispositivo a consultar: ").strip().lower()
     subregion = input("Ingrese la subregion: ").strip()
-
 
     if device_type_input == "web":
         # Datos de autenticación y del dispositivo Web (OTT)
@@ -185,20 +191,18 @@ if __name__ == "__main__":
     if menu_id:
         canales, total = EPG_client.obtener_lineup(menu_id, subregion)
         if canales:
-
-            write_to_excel(canales, total)
-            print("\nDatos guardados en epg_data.xlsx.")
+            # Construir el nombre del archivo
+            file_name = f"{device_type_input}_epg_data_{subregion}.xlsx"
+            write_to_excel(canales, total, file_name)
+            print(f"\nDatos guardados en {file_name}.")
         else:
             print("No se encontraron canales.")
-
             for canal in canales:
                 print(f"Channel: ")
                 print(f"\tNumber: {canal['number']}")
                 print(f"\tName: {canal['name']}")
                 print(f"\tImage: {canal['image']}")
         print(f"\nTotal de canales: {total}")
-
     else:
         print(f"No se encontró la subregion '{subregion}' en la región de argentina.")
-
 
